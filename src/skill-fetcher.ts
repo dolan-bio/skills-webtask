f5029a3ef295387139c5d97bc9fbe9becafac835
Dolan Miu <dolan_miu@hotmail.com>
2017-12-09 18:52:48 +0000

    Remove unused file

--- src/skill-fetcher.ts
+++
@@ -1,8 +0,0 @@
-import { ISkillDocument, Skill } from "./skill-model";
-
-export class SkillsFetcher {
-
-    public async fetch(): Promise<ISkillDocument[]> {
-        return Skill.find();
-    }
-}
