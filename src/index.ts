43602b37b54b0636c8fece60ba6d8f9db7ff33f8
Dolan Miu <dolan_miu@hotmail.com>
2017-12-09 01:46:46 +0000

    Only use skill fetch, no need for fetching image

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,13 @@
 import "babel-polyfill";
 import * as mongoose from "mongoose";
-import * as logger from "winston";
 
-import { SkillAction } from "./action";
+import { Skill } from "./skill-model";
 
 module.exports = async (context, cb) => {
     mongoose.connect(context.secrets.MONGODB_URI);
 
-    logger.debug("Getting skills");
-    const action = new SkillAction({
-        customSearchEngineId: context.secrets.CSE_ID,
-        apiKey: context.secrets.API_KEY,
-    });
+    console.log("Getting skills");
+    const data = await Skill.find();
 
-    action.CombinedObservable$.subscribe((data) => {
-        cb(null, data);
-    }, (err) => {
-        logger.error(err);
-        const errorResponse: ServerError = {
-            message: "Something went wrong with the server",
-        };
-        cb(errorResponse);
-    });
+    cb(null, data);
 };
--- src/skill-model.ts
+++ src/skill-model.ts
@@ -2,10 +2,12 @@ import * as mongoose from "mongoose";
 
 interface ISkillDocument extends mongoose.Document {
     name: string;
+    image: string;
 }
 
 const SkillSchema = new mongoose.Schema({
     name: String,
+    image: String,
 });
 
 const Skill = mongoose.model<ISkillDocument>("Skill", SkillSchema);
